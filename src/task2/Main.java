8e069de378cb0c4ac4ccac287ce5a726d96b5790
darima16 <ms.darima2000@mail.ru>
2020-10-15 16:08:59 +0700

    task1 redone

--- src/task2/Main.java
+++ src/task2/Main.java
@@ -3,11 +3,11 @@ package task2;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-    private int counter;
+    private int counter=0;
     final Object obj = new Object();
-    Thread thread = new Thread();
+    Thread thread2;
 
-    public void increment(){
+    public void mainfunc(){
         Thread thread1 = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -15,87 +15,85 @@ public class Main {
                     TimeUnit.MILLISECONDS.sleep(150);
                 }
                 catch (InterruptedException e) {
-
+                    e.printStackTrace();
                 }
                 for(int i=0; i<5; i++){
                     synchronized (obj) {
+                        counter++;
                         System.out.print("First thread: ");
                         System.out.println(counter);
-                        counter++;
                     }
                     try {
                         TimeUnit.MILLISECONDS.sleep(300);
                     }
                     catch (InterruptedException e) {
-
+                        e.printStackTrace();
                     }
                 }
                 try {
-                    thread.join();
+                    thread2.join();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-
-
-                //System.out.println(counter);
             }
         });
-        System.out.println(thread1.getState());
 
-        Thread thread2 = new Thread(new Runnable() {
+        System.out.println(thread1.getState()); // NEW
+
+        thread2 = new Thread(new Runnable() {
             @Override
             public void run() {
                 for(int i=0; i<10; i++){
                     synchronized (obj) {
-                        System.out.print("Second thread: ");
-                        System.out.println(counter);
+
                         try {
                             TimeUnit.MILLISECONDS.sleep(100);
                         }
                         catch (InterruptedException e) {
-
+                            e.printStackTrace();
                         }
                         counter++;
+                        System.out.print("Second thread: ");
+                        System.out.println(counter);
                     }
                     try {
                         TimeUnit.MILLISECONDS.sleep(300);
                     }
                     catch (InterruptedException e) {
-
+                        e.printStackTrace();
                     }
                 }
-                //System.out.println(counter);
             }
         });
-        thread = thread2;
         thread1.start();
-        System.out.println(thread1.getState());
+        System.out.println(thread1.getState()); //RUNNABLE
         thread2.start();
         try {
             TimeUnit.MILLISECONDS.sleep(465);
         }
         catch (InterruptedException e) {
-            //throw new IllegalStateException("task interrupted", e);
+            throw new IllegalStateException("task interrupted", e);
         }
-        System.out.println(thread1.getState());
-        //System.out.println(thread2.getState());
+
+        System.out.println(thread1.getState()); //BLOCKED
+
         try {
             TimeUnit.MILLISECONDS.sleep(2100);
         }
         catch (InterruptedException e) {
-            //throw new IllegalStateException("task interrupted", e);
+            throw new IllegalStateException("task interrupted", e);
         }
-        System.out.println(thread1.getState());
+        System.out.println(thread1.getState()); //WAITING
         try {
             thread2.join();
             thread1.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println(thread1.getState());
+        System.out.println(thread1.getState()); //TERMINATED
     }
     public static void main(String[] args) {
         Main main = new Main();
-        main.increment();
+        main.mainfunc();
     }
 }
